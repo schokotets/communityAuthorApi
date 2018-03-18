8355beb71748a1aa498b2a0ac140401d70d7a0a0
SchokoTets <moritzleni@gmail.com>
2018-03-18 15:06:31 +0100

    Replaced "Example App" with "Server"

--- server.js
+++ server.js
@@ -28,6 +28,6 @@ var server = app.listen(8081, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log("Example app listening at http://%s:%s", host, port);
+  console.log("Server listening at http://%s:%s", host, port);
 
 });
