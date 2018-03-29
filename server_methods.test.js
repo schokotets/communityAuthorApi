b67279f657c081e23db1ecff0e01348030152505
SchokoTets <moritzleni@gmail.com>
2018-03-29 12:38:32 +0200

    added 'submitting is over, different words' test (for toggle())

--- server_methods.test.js
+++ server_methods.test.js
@@ -179,6 +179,24 @@ describe('toggle()', () => {
 		expect(gameStatus.votingQueue).toEqual({});
 		expect(gameStatus.votingResult).toEqual({});
 	})
+	test('submitting is over, different words', () => {
+		let gameStatus = {
+			voting: false,
+			story: "A story",
+			bannedStrings: ["BANNED_WORD"],
+			votingQueue: {"uuid1": "word0", "uuid2": "word1", "uuid3": "word2"},
+			votingResult: {}
+		}
+		serverFunctions.toggle(gameStatus);
+		expect(gameStatus.voting).toBeTruthy();
+		expect(gameStatus.story).toEqual("A story");
+		expect(gameStatus.bannedStrings).toEqual(["BANNED_WORD"]);
+		expect(gameStatus.votingQueue).toEqual({});
+		expect(gameStatus.votingResult).toEqual({"word0": 0, "word1": 0, "word2": 0});
+	})
+
+	test('submitting is over, banned', () => {})
+	test('submitting is over, same word', () => {})
 })
 
 describe('reset()', () => {
